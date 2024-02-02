35f1a0c7c40f319ef9408a1244b22fe9b7f65071
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-02 03:21:56 +0100

    corrected 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(regex = /\[from:([\w@.]+)\]\s\[to:([\w@.]+)\]\s\[flags:([\w:]+)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
