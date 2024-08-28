fa7153d0b29567705b4857f1c697893e32c10849
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-08-28 09:51:00 +0100

    0x16 task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and return the subscriber count."""
-
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, return 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'by u/mecreal'}
-
-    response = requests.get(url, headers=headers)
-
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return (data['data']['subscribers'])
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        return (0)
+        return 0
