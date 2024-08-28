0f8a1e967c58ef22c923944e90a5440b6441e15c
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-08-28 09:38:53 +0100

    0x16 task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-If the subreddit is invalid, it returns 0.
-"""
+"""Module to query the Reddit API and return the subscriber count."""
+
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'by u/qasqot79'}  # Set a custom User-Agent to avoid issues
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, return 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'by u/mecreal'}
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return (data['data']['subscribers'])
     else:
-        return 0
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
+        return (0)
