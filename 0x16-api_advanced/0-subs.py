08b8cd4b00f83ee08dff95c57d02b7dc7d59fd9c
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-08-28 09:23:58 +0100

    0x16 task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'MyBot/0.0.1'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
