b8251b5d5f8a1e115f562b2f5f46139f0580cabd
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-08-28 14:06:50 +0100

    0x0B_100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,19 @@
 # Puppet manifest to configure SSH client
 
-file { '/home/vagrant/.ssh/config':
+file { '/etc/ssh/ssh-config':
   ensure => present,
 }
 
 file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
-  match => '^#?[\s]*PasswordAuthentication',
+  line => 'PasswordAuthentication no',
+  match => 'PasswordAuthentication yes',
+  replace => true,
 }
 
 file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/school',
-  match => '^#?[\s]*IdentityFile',
+  line => 'IdentityFile ~/.ssh/school',
+  match => '^IdentityFile',
+  ensure => present,
 }
