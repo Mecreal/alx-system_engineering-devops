daf023ba5a52d9a9769e936fac323b22cf914190
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-08-28 10:10:12 +0100

    0x16 task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,8 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'MyBot/0.0.1'}
     params = {'limit': 10}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
